3859146daee91cc314fd651c36185bef9356eb77
张宝冬 <baodong.zhang@argrace.ai>
2020-10-21 17:15:10 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,15 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //函数定义时括号前面要不要有空格
+    "space-before-function-paren": [0, "always"],
+    //强制驼峰法命名
+    "camelcase": 0
   },
   parserOptions: {
     parser: 'babel-eslint'
---
+++ src/views/home/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="homePage">
+    <div class="line"></div>
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item index="1">官方动态</el-menu-item>
+      <el-menu-item index="2">资料库</el-menu-item>
+      <el-menu-item index="3">关于</el-menu-item>
+    </el-menu>
+
+    <img
+      class="background"
+      src="https://zbd-image.oss-cn-hangzhou.aliyuncs.com/eastern-knights/background-image/ogre-back.jpg"
+      alt=""
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  components: {},
+  data() {
+    return {
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import './Home';
+</style>
