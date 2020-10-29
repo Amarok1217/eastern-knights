7fc93044f8417f765c5284b3824f4a5d10e7b474
张宝冬 <baodong.zhang@argrace.ai>
2020-10-29 22:12:01 +0800

    add figureList

---
+++ src/components/figureItem/FigureItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="figureItem"
+    :style="`background-image:url(${figureData.cover})`"
+  >
+    <div class="infoBox">
+      <p class="name">{{figureData.cnName}}</p>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'FigureItem',
+  props: {
+    figureData: {}
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import './FigureItem';
+</style>
--- src/views/wiki/modules/wikiFigures/WikiFigures.vue
+++ src/views/wiki/modules/wikiFigures/WikiFigures.vue
@@ -1,31 +1,49 @@
 <template>
   <div class="wikiFigures flex-column">
     <p class="title">人物</p>
-    <WikiSearchLine @startSearch="initList"></WikiSearchLine>
+    <WikiSearchLine
+      ref="wikiSearchLine"
+      @startSearch="initList"
+    ></WikiSearchLine>
+    <section class="figureList flex-row-center">
+      <FigureItem
+        v-for="(item,index) in figureList"
+        :key="index"
+        :figureData="item"
+      ></FigureItem>
+    </section>
   </div>
 </template>
 <script type="text/ecmascript-6">
 import WikiSearchLine from './WikiSearchLine'
+import FigureItem from '@/components/figureItem/FigureItem'
 import { getFigureList } from '@/request/figures'
 
 export default {
   name: 'WikiFigures',
   components: {
-    WikiSearchLine
+    WikiSearchLine,
+    FigureItem
   },
   data() {
-    return {}
+    return {
+      figureList: []
+    }
   },
-  mounted() {
-    this.initList()
-  },
+  mounted() {},
   methods: {
     initList() {
+      console.log(this.$refs)
       let params = {
-        id: 0
+        wave: this.$refs.wikiSearchLine.wave,
+        race: this.$refs.wikiSearchLine.race,
+        role: this.$refs.wikiSearchLine.role,
+        faction: this.$refs.wikiSearchLine.faction
       }
+      console.log(params)
       getFigureList(params).then((res) => {
-        console.log(res)
+        this.figureList = res.data.data
+        console.log(res.data.data)
       })
     }
   }
