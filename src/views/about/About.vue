0bb84a2ad475f085eed4f0fa928093c52d5e6341
张宝冬 <baodong.zhang@argrace.ai>
2020-11-02 19:18:06 +0800

    fix

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -4,6 +4,12 @@
     <p class="info">联系方式：amarok1217@gmail.com</p>
     <p class="info">欢迎提出意见和建议</p>
     <p class="info">欢迎投稿优秀摄影作品</p>
+    <el-button
+      class="adviceBtn"
+      size="small"
+      type="primary"
+      @click="goAdvice"
+    >我有建议</el-button>
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -11,6 +17,11 @@ export default {
   name: 'About',
   data() {
     return {}
+  },
+  methods: {
+    goAdvice() {
+      window.open('https://github.com/Amarok1217/eastern-knights/issues')
+    }
   }
 }
 </script>
